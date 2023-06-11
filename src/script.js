527294400c9996913423d6fb11c9ecfd5c0cccbd
alex.bortnitskyi <alexey@neonpilots.co.uk>
2023-06-11 14:02:12 +0200

    23_blender custom model

--- src/script.js
+++ src/script.js
@@ -1,196 +1,141 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader'
-import * as dat from "lil-gui";
-/**
- * Debug
- */
-const gui = new dat.GUI();
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import * as dat from 'lil-gui'
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI()
+
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 /**
- * Textures
+ * Models
  */
-const textureLoader = new THREE.TextureLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const dracoLoader = new DRACOLoader();
-const gltfLoader = new GLTFLoader()
-
+const dracoLoader = new DRACOLoader()
 dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
- 
+
 let mixer = null
-gltfLoader.load(
-  // "/models/Duck/glTF/Duck.gltf",
-  // "/models/Duck/glTF-Draco/Duck.gltf",
-  // "/models/FlightHelmet/glTF/FlightHelmet.gltf",
-  "/models/Fox/glTF/Fox.gltf",
-  (gltf) => {
-    console.log("success");
-    console.log(gltf);
-    // scene.add(gltf.scene.children[0]);
 
-    // gltf.scene.children.map((mesh) => scene.add(mesh)) // bug
-
-    // WORKS BUT IT's CAN BE BAD
-    // while(gltf.scene.children.length) {
-    //   scene.add(gltf.scene.children[0])
-    // }
-    // WORKS BUT IT's CAN BE BAD
-
-    // const children = [...gltf.scene.children] // WORKS GREATE
-
-    // children.forEach((child) => scene.add(child))
-
-    // HE IS MUTHERFUCKER 🤡
-
-    // FOX SCALE
-
-    gltf.scene.scale.set(.025, .025, .025)
-
-    // FOX Animation
-    mixer = new THREE.AnimationMixer(gltf.scene)
-    const action = mixer.clipAction(gltf.animations[1])
-
-    action.play()
-
-    scene.add(gltf.scene); // gltf.scene - is THREEJS group
-
-
-  },
-  (progress) => {
-    console.log("progress");
-    console.log(progress);
-  },
-  (error) => {
-    console.log("error");
-    console.log(error);
-  }
-);
-const environmentMapTexture = cubeTextureLoader.load([
-  "/textures/environmentMaps/0/px.png",
-  "/textures/environmentMaps/0/nx.png",
-  "/textures/environmentMaps/0/py.png",
-  "/textures/environmentMaps/0/ny.png",
-  "/textures/environmentMaps/0/pz.png",
-  "/textures/environmentMaps/0/nz.png",
-]);
+gltfLoader.load("/models/burger.gltf", (gltf) => {
+    console.log(gltf)
+  scene.add(gltf.scene);
+});
 
 /**
  * Floor
  */
 const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial({
-    color: "#777777",
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
-  })
-);
-floor.receiveShadow = true;
-floor.rotation.x = -Math.PI * 0.5;
-scene.add(floor);
+    new THREE.PlaneGeometry(50, 50),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
 
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.camera.left = -7;
-directionalLight.shadow.camera.top = 7;
-directionalLight.shadow.camera.right = 7;
-directionalLight.shadow.camera.bottom = -7;
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(-3, 3, 3);
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(- 8, 4, 8)
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 1, 0)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 let previousTime = 0
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime
-  previousTime = elapsedTime
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
-  // Update mixer
-  mixer !== null && mixer.update(deltaTime);
+    if(mixer)
+    {
+        mixer.update(deltaTime)
+    }
 
-  // Update controls
-  controls.update();
+    // Update controls
+    controls.update()
 
-  // Render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
