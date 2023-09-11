fdc603e48f7fac6ead821c40ff9bd2495b6a489e
alex.bortnitskyi <alexey@neonpilots.co.uk>
2023-09-11 13:51:15 +0200

    48_Text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,42 +1,104 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Center,
+  OrbitControls,
+  Text3D,
+  useMatcapTexture,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
-import { Suspense, lazy } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const Hamburger = lazy(() => import("./Hamburger"));
-const Helmet = lazy(() => import("./Helmet"));
-const Placeholder = lazy(() => import("./Placeholder"));
-const Fox = lazy(() => import("./Fox"));
+// import { TorusGeometry } from "three";
+
+// const torusGeometry = new TorusGeometry(1, 0.6, 16, 32);
 
 export default function Experience() {
+
+  const [ matcapTexture ] = useMatcapTexture("4C4C4C_D2D2D2_8F8F8F_ACACAC", 256);
+    const [donutMT] = useMatcapTexture("4F439F_A28BE5_8570D6_7765C9", 256);
+    // const donutGRef = useRef()
+    const donutsRef = useRef([])
+    const [torusGeometry, setTorusGeometry] = useState()
+    const [torusMaterial, setTorusMaterial] = useState()
+    
+    // useEffect(() => {
+    //   torusMaterial.matcap = donutMT
+    //   torusMaterial.needsUpdate = true
+    // }, [])
+
+    // useFrame((state, delta) => {
+    //   for(const donut of donutGRef.current.children) {
+    //     donut.rotation.y += delta * .2
+    //   }
+    // })
+
+    useFrame((state, delta) => {
+      for (const donut of donutsRef.current) {
+        donut.rotation.y += delta * 0.2;
+      }
+    })
+
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
-      <directionalLight
-        castShadow
-        position={[1, 2, 3]}
-        intensity={1.5}
-        shadow-normalBias={0.05}
-      />
-      <ambientLight intensity={0.5} />
+      <torusGeometry args={[1, 0.6, 16, 32]} ref={setTorusGeometry} />
+      <meshMatcapMaterial matcap={donutMT} ref={setTorusMaterial} />
+      <Center>
+        <Text3D
+          size={0.7}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.025}
+          bevelOffset={0}
+          bevelSegments={5}
+          font={"./fonts/helvetiker_regular.typeface.json"}
+        >
+          Oleksii Bortnytskyi
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      </Center>
 
-      <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+      {/* <group ref={donutGRef}>
+        {[
+          [...Array(100)].map((item, id) => (
+            <mesh
+              position={[
+                (Math.random() - 0.5) * 10,
+                (Math.random() - 0.5) * 10,
+                (Math.random() - 0.5) * 10,
+              ]}
+              rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+              scale={0.2 + Math.random() * 0.2}
+              key={id}
+              geometry={torusGeometry}
+              material={torusMaterial}
+            />
+          )),
+        ]}
+      </group> */}
 
-      <Suspense fallback={<Placeholder scale={[2, 2, 1]} />}>
-        {/* <Helmet /> */}
-        {/* <Hamburger /> */}
-        <Fox />
-      </Suspense>
+      {[
+        [...Array(100)].map((item, id) => (
+          <mesh
+            ref={(element) => donutsRef.current[id] = element}
+            position={[
+              (Math.random() - 0.5) * 10,
+              (Math.random() - 0.5) * 10,
+              (Math.random() - 0.5) * 10,
+            ]}
+            rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+            scale={0.2 + Math.random() * 0.2}
+            key={id}
+            geometry={torusGeometry}
+            material={torusMaterial}
+          />
+        )),
+      ]}
     </>
   );
 }
